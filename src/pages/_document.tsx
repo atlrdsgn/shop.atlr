c322172111f0ff1f1ae273aa3fc59c9515c08df7
chvndler <chvndler.ch@icloud.com>
2023-01-28 01:29:13 -0500

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,26 +1,20 @@
+/** @format */
+
 import Document, {DocumentContext, Head, Html, Main, NextScript} from 'next/document'
-import * as React from 'react'
 import {getCssText, reset} from 'theme.config'
 
-const getCssAndReset = () => {
-  const css = getCssText()
-  reset()
-  return css
-}
-
-class MyDocument extends Document {
+class App extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx)
     return {...initialProps}
   }
-
   render() {
     return (
-      <Html lang='en'>
+      <Html lang={'en'}>
         <Head>
-          <style id='stitches' dangerouslySetInnerHTML={{__html: getCssAndReset()}} />
+          <style id={'stitches'} dangerouslySetInnerHTML={{__html: getCssAndReset()}} />
         </Head>
-        <body style={{opacity: 0}}>
+        <body style={{opacity: '0'}}>
           <Main />
           <NextScript />
         </body>
@@ -29,4 +23,10 @@ class MyDocument extends Document {
   }
 }
 
-export default MyDocument
+export default App
+
+const getCssAndReset = () => {
+  const css = getCssText()
+  reset()
+  return css
+}

3e03cd37cc2e55bbab91869d09cf48ae71c0f9da
chvndler <chvndler.ch@icloud.com>
2023-02-06 00:15:51 -0500

    up

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+import {Meta} from '@/components/common/meta'
+import {PageLayout} from '@/components/layout/page'
+
+export default function Page() {
+  return (
+    <PageLayout variant={'Page'}>
+      <Meta />
+    </PageLayout>
+  )
+}
